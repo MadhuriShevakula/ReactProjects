4c29fb6f4bbba5997ce4aba8982fba9ea31df7d7
Madhuri <madhurishevakula@gmail.com>
2024-04-26 19:26:40 +0530

    add accordion random-color star-rating projects

---
+++ accordion/src/App.js
@@ -0,0 +1,21 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Accordian from './components/accordian/index';
+import RandomColor from './components/random-color';
+import StarRating from './components/star-rating';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Accordian/> */}
+
+      {/* <RandomColor/> */}
+  
+  
+      <StarRating noOfStars={10}/>
+    </div>
+  );
+}
+
+export default App;
---
+++ accordion/src/components/accordian/index.jsx
@@ -0,0 +1,70 @@
+// single selection
+// multiple selection
+
+import { useState } from "react"
+import data from "./data"
+import './styles.css'
+
+
+export default function Accordian(){
+    
+    const [selected, setSelected] = useState(null);
+    const [enableMultiSelection, setEnableMultiSelection] = useState(false)
+    const [multiple, setMultiple] = useState([])
+
+    const handleSingleSelection =(getCurrentId)=>{
+        // console.log(getCurrentId);
+        setSelected((prevSelected)=> (getCurrentId === prevSelected ? null : getCurrentId))
+    }
+
+
+    const handleMultiSelection =(getCurrentId)=>{
+        setMultiple((prevMultiple) => {
+            const copyMultiple = [...prevMultiple];
+            const findIndexOfCurrentId = copyMultiple.indexOf(getCurrentId);
+            
+            if(findIndexOfCurrentId === -1) 
+                copyMultiple.push(getCurrentId)
+            else
+                copyMultiple.splice(findIndexOfCurrentId, 1)
+            return copyMultiple
+            
+        });
+    }
+
+    console.log(selected, multiple)
+    return <div className="wrapper">
+        <button onClick={() => setEnableMultiSelection((prev) =>!prev)}>Enable MultiSelection</button>
+        <div className="accordian">
+            {
+                data && data.length > 0 ? (
+                    data.map((dataItem) => (
+                    <div 
+                        key={dataItem.id}    
+                        onClick={
+                            enableMultiSelection 
+                               ? ()=>handleMultiSelection(dataItem.id) 
+                               : ()=>handleSingleSelection(dataItem.id)
+                        }
+                        className="item"
+                     >
+                        <div className="title">
+                            <h3>{dataItem.question}</h3>
+                            <span>+</span>
+                        </div>
+
+                        {
+                            enableMultiSelection ?
+                            multiple.indexOf(dataItem.id) !== -1 && 
+                            <div className="content">{dataItem.answer}</div> 
+                            :  selected === dataItem.id && (
+                                <div className="content">{dataItem.answer}</div>
+                            )
+                        }
+                    </div>
+                    ))) : (
+                    <div>No data Found!</div>)
+            }
+        </div>
+    </div>
+}
